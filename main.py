6159cf5b6211f0c536ab88a7fa143873cbcec015
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-06-22 21:26:07 +0530

    ..

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from website import create_app
 
 
-app=create_app() #calling create app function and assign the returned value to variable app
+app = create_app() #calling create app function and assign the returned value to variable app
 if __name__ == '__main__':  # checks if the script is being run directly as the main entry point of the program
     app.run(debug=True)      # the .run method starts a development server and listens for incoming requests. Setting debug = True, enables debig mode, which provides detailed error msg and automatic reloading of the application when changes are made.
       # debug = True helps us from manually re-running the server. 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint  # blueprint is a way to organize and structure a web application into reusable components
+from flask import Blueprint,render_template  # blueprint is a way to organize and structure a web application into reusable components
 
 
 views = Blueprint('views',__name__) # defining a blue print
 
 @views.route('/')
 def home():
-    return "<h1>TEST</h1>"         # 'pass' statement is a placeholder that does nothing.It is just used when a statement that is syntatically required, used as a temporary placeholder for code that will be implemented later.
+    return render_template("home.html")         # 'pass' statement is a placeholder that does nothing.It is just used when a statement that is syntatically required, used as a temporary placeholder for code that will be implemented later.
